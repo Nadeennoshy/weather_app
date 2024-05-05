d049579920e1e300a256a2981a79aa6851cc7ba2
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-05-05 04:27:42 +0300

    Integrate cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
 import 'package:weather_app/widgets/weather_info_body.dart';
@@ -11,20 +14,37 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather App'),
-        actions:  [
-           Padding(
-            padding:  const EdgeInsets.only(right: 10),
-            child:  IconButton(
-              onPressed: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
-                  return const SearchView();
-                },),);
-              },
-              icon: const Icon(Icons.search,)),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const SearchView();
+                      },
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.search,
+                )),
           ),
         ],
       ),
-      body: weatherModel==null ? const NoWeatherBody():const WeatherInfoBody(),
+      body: BlocBuilder<GetWeatherCubit,WeatherState>(
+        builder: (context, state) {
+          if (state is InitialWeatherState) {
+            return const NoWeatherBody();
+          } else if (state is WeatherLoadedState) {
+            return const WeatherInfoBody();
+          } else {
+            return const Text('OOPS, There was an error try again !');
+          }
+        },
+      ),
+      // body: weatherModel==null ? const NoWeatherBody():const WeatherInfoBody(),
       // body: const WeatherInfoBody(),
       // body: const NoWeatherBody(),
     );
